3ecf568aa8ada4e360e412918373ab9193a3d762
Alejandro Gómez <aldajo92mac@AlejandroMacM1.attlocal.net>
2022-09-29 19:47:15 -0400

    first UI Version

--- AndroidApp/app/src/main/java/com/aldajo92/mardanrobot/ui/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/aldajo92/mardanrobot/ui/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.accessibility.AccessibilityRecordCompat.setSource
 import com.aldajo92.mardanrobot.JoyStick
 import com.aldajo92.mardanrobot.presentation.MainViewModel
 import com.github.niqdev.mjpeg.DisplayMode
@@ -46,60 +47,66 @@ class MainActivity : ComponentActivity() {
 fun BodyComposable(
     inputStream: MjpegInputStream? = null
 ) {
-    Box(Modifier.fillMaxSize()) {
+    Column(Modifier.fillMaxSize()) {
         VideoStreamView(
-            modifier = Modifier.align(Alignment.TopCenter),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .width(300.dp)
+                .weight(1f)
+                .padding(10.dp),
             inputStream = inputStream
         )
-        JoyStick(
-            modifier = Modifier
-                .padding(30.dp)
-                .align(Alignment.BottomStart),
-            size = 100.dp,
-            dotSize = 30.dp,
-            backgroundComposable = {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .background(Color.Red)
-                )
-            },
-            dotComposable = {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .background(Color.Black)
-                )
+        Box(modifier = Modifier.fillMaxWidth()) {
+            JoyStick(
+                modifier = Modifier
+                    .padding(30.dp)
+                    .align(Alignment.BottomStart),
+                size = 100.dp,
+                dotSize = 30.dp,
+                backgroundComposable = {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                    )
+                },
+                dotComposable = {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                            .background(Color.Black)
+                    )
+                }
+            ) { x, y ->
+                Log.d("JoyStick1", "$x, $y")
             }
-        ) { x, y ->
-            Log.d("JoyStick1", "$x, $y")
-        }
-        JoyStick(
-            modifier = Modifier
-                .padding(30.dp)
-                .align(Alignment.BottomEnd),
-            size = 100.dp,
-            dotSize = 30.dp,
-            backgroundComposable = {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .background(Color.Red)
-                )
-            },
-            dotComposable = {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .background(Color.Black)
-                )
+            JoyStick(
+                modifier = Modifier
+                    .padding(30.dp)
+                    .align(Alignment.BottomEnd),
+                size = 100.dp,
+                dotSize = 30.dp,
+                backgroundComposable = {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                    )
+                },
+                dotComposable = {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                            .background(Color.Black)
+                    )
+                }
+            ) { x, y ->
+                Log.d("JoyStick2", "$x, $y")
             }
-        ) { x, y ->
-            Log.d("JoyStick2", "$x, $y")
         }
     }
 }
@@ -112,17 +119,17 @@ fun VideoStreamView(
     inputStream: MjpegInputStream? = null
 ) {
     AndroidView(
-        modifier = modifier
-            .height(100.dp)
-            .width(200.dp),
+        modifier = modifier,
         factory = { context ->
             MjpegSurfaceView(context, null).apply {
-                if (inputStream != null) setSource(inputStream)
                 setDisplayMode(DisplayMode.BEST_FIT)
                 showFps(true)
             }
         },
         update = { view ->
+            with(view) {
+                if (inputStream != null) setSource(inputStream) else stopPlayback()
+            }
             // View's been inflated or state read in this block has been updated
             // Add logic here if necessary
 
